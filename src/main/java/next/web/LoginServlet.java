a572280ef35ea49b6fa88363345e13ab5a8724c2
rlatls1997 <rlatls1997@gmail.com>
2023-03-26 17:00:08 +0900

    feat : 6장 - 로그인/로그아웃 기능 실습
    - 로그인 기능 실습내용 구현

---
+++ src/main/java/next/web/LoginServlet.java
@@ -0,0 +1,41 @@
+package next.web;
+
+import java.io.IOException;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import core.db.DataBase;
+import next.model.User;
+
+@WebServlet("/user/login")
+public class LoginServlet extends HttpServlet {
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String userId = request.getParameter("userId");
+		String password = request.getParameter("password");
+
+		User user = DataBase.findUserById(userId);
+
+		if (Objects.isNull(user)) {
+			throw new NoSuchElementException("update user not exist. userId:" + userId);
+		}
+
+		if (password.equals(user.getPassword())) {
+			HttpSession session = request.getSession();
+			session.setAttribute("user", user);
+			response.sendRedirect("/user/list");
+			return;
+		}
+
+		throw new IllegalArgumentException("wrong login password. userId:" + userId + ",password:" + password);
+	}
+}
