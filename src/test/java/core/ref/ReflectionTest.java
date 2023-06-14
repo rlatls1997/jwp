3829df1939454000d98657ac3fc8ccac83c363e6
rlatls1997 <rlatls1997@gmail.com>
2023-06-14 16:49:19 +0900

    test : 생성자가 있는 클래스의 인스턴스 생성하기

--- src/test/java/core/ref/ReflectionTest.java
+++ src/test/java/core/ref/ReflectionTest.java
@@ -1,5 +1,9 @@
 package core.ref;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,23 +12,27 @@ import next.model.Question;
 import next.model.User;
 
 public class ReflectionTest {
-    private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+	private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
-    @Test
-    public void showClass() {
-        Class<Question> clazz = Question.class;
-        logger.debug(clazz.getName());
-    }
-    
-    @Test
-    public void newInstanceWithConstructorArgs() {
-        Class<User> clazz = User.class;
-        logger.debug(clazz.getName());
-    }
-    
-    @Test
-    public void privateFieldAccess() {
-        Class<Student> clazz = Student.class;
-        logger.debug(clazz.getName());
-    }
+	@Test
+	public void showClass() {
+		Class<Question> clazz = Question.class;
+		logger.debug(clazz.getName());
+	}
+
+	@Test
+	public void newInstanceWithConstructorArgs() throws InvocationTargetException, InstantiationException, IllegalAccessException {
+		Class<User> clazz = User.class;
+
+		User user = (User)clazz.getDeclaredConstructors()[0].newInstance("userId", "password", "name", "email");
+		logger.debug("user : {}", user);
+
+		logger.debug(clazz.getName());
+	}
+
+	@Test
+	public void privateFieldAccess() {
+		Class<Student> clazz = Student.class;
+		logger.debug(clazz.getName());
+	}
 }
