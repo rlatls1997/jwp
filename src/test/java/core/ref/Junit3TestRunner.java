53bbc17311c9fe03a09396dfbe5bcb957610dd75
rlatls1997 <rlatls1997@gmail.com>
2023-06-14 16:35:36 +0900

    test : "test"로 시작하는 메소드 실행하기

--- src/test/java/core/ref/Junit3TestRunner.java
+++ src/test/java/core/ref/Junit3TestRunner.java
@@ -1,11 +1,21 @@
 package core.ref;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class Junit3TestRunner {
-    @Test
-    public void run() throws Exception {
-        Class<Junit3Test> clazz = Junit3Test.class;
-
-    }
+	@Test
+	public void run() throws Exception {
+		Class<Junit3Test> clazz = Junit3Test.class;
+		Arrays.stream(clazz.getMethods()).forEach(method -> {
+			if (method.getName().startsWith("test")) {
+				try {
+					method.invoke(clazz.newInstance());
+				} catch (Exception exception) {
+					exception.printStackTrace();
+				}
+			}
+		});
+	}
 }
